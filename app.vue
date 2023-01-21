c8d7b21ca18fced9514b1b56dfe4cf16316c58d4
Jose Luis Cabrera <cabrera@inkuba.com>
2023-01-21 19:50:39 -0300

    feat: TrelloBoard component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-hidden">
+    <TrelloBoard />
   </div>
 </template>
---
+++ components/TrelloBoard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { Column } from "@/types"
+import { nanoid } from "nanoid"
+const columns = ref<Column[]>([
+    {
+        id: nanoid(),
+        title: "Backlog",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Create marketing landing page",
+                createAt: new Date()
+
+            }
+        ]
+    },
+    {
+        id: nanoid(),
+        title: "Complete",
+        tasks: [],
+    },
+    {
+        id: nanoid(),
+        title: "I Progress",
+        tasks: [],
+    }
+])
+
+</script>
+
+<template>
+    <div class=" flex gap-4 overflow-x-auto items-start">
+        <div v-for="column in columns" :key="column.id" class="bg-gray-200 p-5 rounded min-w-[250px]">
+            <header>
+                {{ column.title }}
+            </header>
+            <p v-for="task in column.tasks" :key="task.id">
+                {{ task.title }}
+            </p>
+        </div>
+    </div>
+</template>
